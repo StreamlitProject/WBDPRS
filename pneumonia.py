5bfbc84915b6f9be341c51e4aeaae04be930c748
StreamlitProject <100086766+StreamlitProject@users.noreply.github.com>
2022-04-16 18:12:33 +0530

    Update pneumonia.py

--- pneumonia.py
+++ pneumonia.py
@@ -38,7 +38,7 @@ def pneumonia():
     elif selected1=='Upload Image':
         uploaded_file = st.file_uploader("Choose a file")
         if uploaded_file is not None:
-            t=uploaded_file.getvalue()
+            t=open(uploaded_file,'rb').getvalue()
             img = image.load_img(t,target_size=(224,224))
             x = image.img_to_array(img)
             x = np.expand_dims(x,axis=0)
