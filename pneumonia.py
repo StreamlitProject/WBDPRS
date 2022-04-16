5d93ea50a3f1666dcd3d77fad2ee6c0a1438cefc
StreamlitProject <100086766+StreamlitProject@users.noreply.github.com>
2022-04-16 18:25:13 +0530

    Update pneumonia.py

--- pneumonia.py
+++ pneumonia.py
@@ -12,7 +12,7 @@ from PIL import Image
 #besturl='https://drive.google.com/uc?id=' + besturl.split('/')[-2]
 
 model = load_model('model_vgg16.h5')
-def Load_image(image_file):
+def LOAD_image(image_file):
     img = Image.open(image_file)
     return img
 
@@ -38,8 +38,8 @@ def pneumonia():
     elif selected1=='Upload Image':
         uploaded_file = st.file_uploader("Choose a file")
         if uploaded_file is not None:
-            t=uploaded_file.read()
-            img = image.load_img(Image.BytesIO(t),target_size=(224,224))
+            t=LOAD_image(uploaded_file)
+            img = image.load_img(t,target_size=(224,224))
             x = image.img_to_array(img)
             x = np.expand_dims(x,axis=0)
             img_data = preprocess_input(x)
