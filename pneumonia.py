1dcbe4397829263b3b9b9d994ee154e8c2507f78
StreamlitProject <100086766+StreamlitProject@users.noreply.github.com>
2022-04-16 18:04:44 +0530

    Update pneumonia.py

--- pneumonia.py
+++ pneumonia.py
@@ -12,16 +12,16 @@ from PIL import Image
 #besturl='https://drive.google.com/uc?id=' + besturl.split('/')[-2]
 
 model = load_model('model_vgg16.h5')
-
+def LOAD_image(image_file):
+    img = Image.open(image_file)
+    return img
 
 def pneumonia():
     selected1 = option_menu(None, ['Camera','Upload Image'],
                             icons=['camera','image'], 
                             menu_icon="cast", default_index=0, orientation="horizontal",
                             styles={"container": {"padding": "0!important", "background-color": "#fafafa"},"icon": {"color": "black", "font-size": "15px"}, "nav-link": {"font-size": "15px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},"nav-link-selected": {"background-color": "#6cdacf"},})
-    def LOAD_image(image_file):
-        img = Image.open(image_file)
-        return img
+
     if selected1=='Camera':
         picture = st.camera_input("Take a picture")
         if picture:
@@ -38,7 +38,8 @@ def pneumonia():
     elif selected1=='Upload Image':
         uploaded_file = st.file_uploader("Choose a file")
         if uploaded_file is not None:
-            img = image.load_img(LOAD_image(uploaded_file),target_size=(224,224))
+            t=Load_image(uploaded_file)
+            img = image.load_img(t,target_size=(224,224))
             x = image.img_to_array(img)
             x = np.expand_dims(x,axis=0)
             img_data = preprocess_input(x)
