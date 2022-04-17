cad7761dc2dc833263e65893c783537bca7a2346
StreamlitProject <100086766+StreamlitProject@users.noreply.github.com>
2022-04-17 09:56:11 +0530

    Update pneumonia.py

--- pneumonia.py
+++ pneumonia.py
@@ -16,7 +16,7 @@ from PIL import Image
 def pneumonia():
     model = load_model('model_vgg16.h5')
     def LOAD_image(image_file):
-     img = Image.open(image_file,'rb')
+     img = Image.open(image_file,mode='r')
      return img
     selected1 = option_menu(None, ['Camera','Upload Image'],
                             icons=['camera','image'], 
