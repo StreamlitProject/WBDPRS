4d44ee793ab7ffd575feff91f8e8c0f758e79130
StreamlitProject <100086766+StreamlitProject@users.noreply.github.com>
2022-04-17 11:34:18 +0530

    Update pneumonia.py

--- pneumonia.py
+++ pneumonia.py
@@ -39,7 +39,7 @@ def pneumonia():
         if uploaded_file is not None:
             t=Image.open(uploaded_file)
             img=t.resize((224,224))
-            img_test = image.load_img(img,grayscale=grayscale)
+            img_test = image.load_img(img,grayscale=True)
             x = image.img_to_array(img_test)
             x = np.expand_dims(x,axis=0)
             img_data = preprocess_input(x)
