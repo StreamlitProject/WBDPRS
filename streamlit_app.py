33ea9ae8b784943892dbafa2a7ed7857a2ab34ae
StreamlitProject <100086766+StreamlitProject@users.noreply.github.com>
2022-05-02 09:33:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ import skin as s
 st.set_page_config(
     page_title="WBDPRS",
     page_icon="🤖",
-    layout="wide",
+    layout="centered",
     initial_sidebar_state="expanded",
     menu_items={
         'Get Help': 'https://www.extremelycoolapp.com/help',
