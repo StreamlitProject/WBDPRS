5e4a6dd415eda1d1c786fae3fbb28e3081245293
StreamlitProject <100086766+StreamlitProject@users.noreply.github.com>
2022-05-31 12:42:30 +0530

    Update multidisease.py

--- multidisease.py
+++ multidisease.py
@@ -8,6 +8,8 @@ def multidisease():
         st.markdown("Have A Quick Overlook :eyes:")
         st.subheader("Steps to follow:")
         st.text("Step I: Go down in this webpage, where you find a template to check you prediction \n of disease using your symptoms")
+        st.text("Step II: Select all the five symptoms(similar) that you have from the pre-existed \n drop-down list.")
+        st.text("Step III: Now click on submit")
 
     l1 = ['itching', 'skin_rash', 'nodal_skin_eruptions', 'continuous_sneezing', 'shivering', 'chills', 'joint_pain',
           'stomach_pain', 'acidity', 'ulcers_on_tongue', 'muscle_wasting', 'vomiting', 'burning_micturition',
