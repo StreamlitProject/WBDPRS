a854a24233c68c7ffa5d89fef61788b6015f22de
StreamlitProject <100086766+StreamlitProject@users.noreply.github.com>
2022-06-01 09:02:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,8 +85,8 @@ def set_bg_hack_url():
 	.css-eczf16{{display: none;}} /*hide the link on title*/
 	.css-1xfuh55{{display: none;}} /*hide the link on images*/
 	.css-6awftf{{display: none;}} /*hide the link on images*/
-	.st-bu {{background-color:rgb(109,218,207);}}
-	.st-bp {{font-size: 15px;}}
+	/*.st-bu {{background-color:rgb(109,218,207);}}*/
+	/*.st-bp {{font-size: 15px;}}*/
 	
 	/* this to remove all the things*/
 	 div[data-testid="stToolbar"] {{
