9f79445d4dd368e3e42ff43e8a21df8cab807eff
StreamlitProject <100086766+StreamlitProject@users.noreply.github.com>
2025-10-21 16:28:55 +0530

    Update multidisease.py

--- multidisease.py
+++ multidisease.py
@@ -1,176 +1,84 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+from sklearn.naive_bayes import MultinomialNB
 
+# Cache the data loading to avoid reloading every rerun
+@st.cache_data
+def load_data():
+    train_df = pd.read_csv("Training.csv")
+    test_df = pd.read_csv("Testing.csv")
+    
+    # Map diseases to numbers
+    disease_map = {disease: i for i, disease in enumerate([
+        'Fungal infection', 'Allergy', 'GERD', 'Chronic cholestasis', 'Drug Reaction', 
+        'Peptic ulcer diseae', 'AIDS', 'Diabetes', 'Gastroenteritis', 'Bronchial Asthma', 
+        'Hypertension', 'Migraine', 'Cervical spondylosis', 'Paralysis (brain hemorrhage)', 
+        'Jaundice', 'Malaria', 'Chicken pox', 'Dengue', 'Typhoid', 'hepatitis A', 'Hepatitis B', 
+        'Hepatitis C', 'Hepatitis D', 'Hepatitis E', 'Alcoholic hepatitis', 'Tuberculosis', 
+        'Common Cold', 'Pneumonia', 'Dimorphic hemmorhoids(piles)', 'Heartattack', 'Varicoseveins', 
+        'Hypothyroidism', 'Hyperthyroidism', 'Hypoglycemia', 'Osteoarthristis', 'Arthritis', 
+        '(vertigo) Paroymsal  Positional Vertigo', 'Acne', 'Urinary tract infection', 'Psoriasis', 'Impetigo'
+    ])}
+    
+    train_df.replace({'prognosis': disease_map}, inplace=True)
+    test_df.replace({'prognosis': disease_map}, inplace=True)
+    
+    return train_df, test_df, disease_map
+
+# Cache the model training
+@st.cache_data
+def train_model(train_df, l1):
+    X = train_df[l1]
+    y = np.ravel(train_df[["prognosis"]])
+    gnb = MultinomialNB()
+    gnb.fit(X, y)
+    return gnb
 
 def multidisease():
-    with st.container():
-        st.markdown("Have A Quick Overlook :eyes:")
-        st.text("Step I: Go down in this webpage, where you find a template to check you prediction \n of disease by selecting your symptoms")
-        st.text("Step II: Select all the five symptoms(mandatory) that you have from the pre-existed \n drop-down list.")
-        st.text("Step III: Now click on submit.\n")
-        st.markdown("That's cool :sunglasses:")
-        st.info("Note: You find  your approximate disease that matches your symptoms")
-        st.text("\n\n\n")
-        
-
-    l1 = ['itching', 'skin_rash', 'nodal_skin_eruptions', 'continuous_sneezing', 'shivering', 'chills', 'joint_pain',
-          'stomach_pain', 'acidity', 'ulcers_on_tongue', 'muscle_wasting', 'vomiting', 'burning_micturition',
-          'spotting_ urination', 'fatigue', 'weight_gain', 'anxiety', 'cold_hands_and_feets', 'mood_swings',
-          'weight_loss', 'restlessness', 'lethargy', 'patches_in_throat', 'irregular_sugar_level', 'cough',
-          'high_fever', 'sunken_eyes', 'breathlessness', 'sweating', 'dehydration', 'indigestion', 'headache',
-          'yellowish_skin', 'dark_urine', 'nausea', 'loss_of_appetite', 'pain_behind_the_eyes', 'back_pain',
-          'constipation', 'abdominal_pain', 'diarrhoea', 'mild_fever', 'yellow_urine', 'yellowing_of_eyes',
-          'acute_liver_failure', 'fluid_overload', 'swelling_of_stomach', 'swelled_lymph_nodes', 'malaise',
-          'blurred_and_distorted_vision', 'phlegm', 'throat_irritation', 'redness_of_eyes', 'sinus_pressure',
-          'runny_nose', 'congestion', 'chest_pain', 'weakness_in_limbs', 'fast_heart_rate',
-          'pain_during_bowel_movements', 'pain_in_anal_region', 'bloody_stool', 'irritation_in_anus', 'neck_pain',
-          'dizziness', 'cramps', 'bruising', 'obesity', 'swollen_legs', 'swollen_blood_vessels', 'puffy_face_and_eyes',
-          'enlarged_thyroid', 'brittle_nails', 'swollen_extremeties', 'excessive_hunger', 'extra_marital_contacts',
-          'drying_and_tingling_lips', 'slurred_speech', 'knee_pain', 'hip_joint_pain', 'muscle_weakness', 'stiff_neck',
-          'swelling_joints', 'movement_stiffness', 'spinning_movements', 'loss_of_balance', 'unsteadiness',
-          'weakness_of_one_body_side', 'loss_of_smell', 'bladder_discomfort', 'foul_smell_of urine',
-          'continuous_feel_of_urine', 'passage_of_gases', 'internal_itching', 'toxic_look_(typhos)', 'depression',
-          'irritability', 'muscle_pain', 'altered_sensorium', 'red_spots_over_body', 'belly_pain',
-          'abnormal_menstruation', 'dischromic _patches', 'watering_from_eyes', 'increased_appetite', 'polyuria',
-          'family_history', 'mucoid_sputum', 'rusty_sputum', 'lack_of_concentration', 'visual_disturbances',
-          'receiving_blood_transfusion', 'receiving_unsterile_injections', 'coma', 'stomach_bleeding',
-          'distention_of_abdomen', 'history_of_alcohol_consumption', 'fluid_overload', 'blood_in_sputum',
-          'prominent_veins_on_calf', 'palpitations', 'painful_walking', 'pus_filled_pimples', 'blackheads', 'scurring',
-          'skin_peeling', 'silver_like_dusting', 'small_dents_in_nails', 'inflammatory_nails', 'blister',
-          'red_sore_around_nose', 'yellow_crust_ooze']
-
-    disease = ['Fungal infection', 'Allergy', 'GERD', 'Chronic cholestasis', 'Drug Reaction', 'Peptic ulcer diseae',
-               'AIDS', 'Diabetes', 'Gastroenteritis', 'Bronchial Asthma', 'Hypertension', ' Migraine',
-               'Cervical spondylosis', 'Paralysis (brain hemorrhage)', 'Jaundice', 'Malaria', 'Chicken pox', 'Dengue',
-               'Typhoid', 'hepatitis A', 'Hepatitis B', 'Hepatitis C', 'Hepatitis D', 'Hepatitis E',
-               'Alcoholic hepatitis', 'Tuberculosis', 'Common Cold', 'Pneumonia', 'Dimorphic hemmorhoids(piles)',
-               'Heartattack', 'Varicoseveins', 'Hypothyroidism', 'Hyperthyroidism', 'Hypoglycemia', 'Osteoarthristis',
-               'Arthritis', '(vertigo) Paroymsal  Positional Vertigo', 'Acne', 'Urinary tract infection', 'Psoriasis',
-               'Impetigo']
-
-    #testurl = 'https://drive.google.com/file/d/1cY89ARWHkUviL34hI8jRHlu5cRvn_RZH/view?usp=sharing'
-    #testurl = 'https://drive.google.com/uc?id=' + testurl.split('/')[-2]
-    testdf = pd.read_csv("Testing.csv")
-    #trainurl = 'https://drive.google.com/file/d/1dSryy7kA39UECxjxGoLIixRgNXDMnj1-/view?usp=sharing'
-    #trainurl = 'https://drive.google.com/uc?id=' + trainurl.split('/')[-2]
-    traindf = pd.read_csv("Training.csv")
-
-    l2 = []
-    for x in range(0, len(l1)):
-        l2.append(0)
-    # print(l2)
-    traindf.replace({'prognosis': {'Fungal infection': 0, 'Allergy': 1, 'GERD': 2, 'Chronic cholestasis': 3,
-                                   'Drug Reaction': 4, 'Peptic ulcer diseae': 5, 'AIDS': 6, 'Diabetes ': 7,
-                                   'Gastroenteritis': 8, 'Bronchial Asthma': 9, 'Hypertension ': 10, 'Migraine': 11,
-                                   'Cervical spondylosis': 12, 'Paralysis (brain hemorrhage)': 13, 'Jaundice': 14,
-                                   'Malaria': 15, 'Chicken pox': 16, 'Dengue': 17, 'Typhoid': 18, 'hepatitis A': 19,
-                                   'Hepatitis B': 20, 'Hepatitis C': 21, 'Hepatitis D': 22, 'Hepatitis E': 23,
-                                   'Alcoholic hepatitis': 24, 'Tuberculosis': 25, 'Common Cold': 26, 'Pneumonia': 27,
-                                   'Dimorphic hemmorhoids(piles)': 28, 'Heart attack': 29, 'Varicose veins': 30,
-                                   'Hypothyroidism': 31, 'Hyperthyroidism': 32, 'Hypoglycemia': 33,
-                                   'Osteoarthristis': 34, 'Arthritis': 35,
-                                   '(vertigo) Paroymsal  Positional Vertigo': 36, 'Acne': 37,
-                                   'Urinary tract infection': 38, 'Psoriasis': 39, 'Impetigo': 40}}, inplace=True)
-    X = traindf[l1]
-    y = traindf[["prognosis"]]
-    np.ravel(y)
-
-    testdf.replace({'prognosis': {'Fungal infection': 0, 'Allergy': 1, 'GERD': 2, 'Chronic cholestasis': 3,
-                                  'Drug Reaction': 4, 'Peptic ulcer diseae': 5, 'AIDS': 6, 'Diabetes ': 7,
-                                  'Gastroenteritis': 8, 'Bronchial Asthma': 9, 'Hypertension ': 10, 'Migraine': 11,
-                                  'Cervical spondylosis': 12, 'Paralysis (brain hemorrhage)': 13, 'Jaundice': 14,
-                                  'Malaria': 15, 'Chicken pox': 16, 'Dengue': 17, 'Typhoid': 18, 'hepatitis A': 19,
-                                  'Hepatitis B': 20, 'Hepatitis C': 21, 'Hepatitis D': 22, 'Hepatitis E': 23,
-                                  'Alcoholic hepatitis': 24, 'Tuberculosis': 25, 'Common Cold': 26, 'Pneumonia': 27,
-                                  'Dimorphic hemmorhoids(piles)': 28, 'Heart attack': 29, 'Varicose veins': 30,
-                                  'Hypothyroidism': 31, 'Hyperthyroidism': 32, 'Hypoglycemia': 33,
-                                  'Osteoarthristis': 34, 'Arthritis': 35, '(vertigo) Paroymsal  Positional Vertigo': 36,
-                                  'Acne': 37, 'Urinary tract infection': 38, 'Psoriasis': 39, 'Impetigo': 40}},
-                   inplace=True)
-    X_test = testdf[l1]
-    y_test = testdf[["prognosis"]]
-    np.ravel(y_test)
-
-    from sklearn.naive_bayes import MultinomialNB
-    gnb = MultinomialNB()
-    gnb = gnb.fit(X, np.ravel(y))
-    from sklearn.metrics import accuracy_score
-    y_pred = gnb.predict(X_test)
-    # print(accuracy_score(y_test, y_pred))
-    # print(accuracy_score(y_test, y_pred, normalize=False))
+    st.markdown("## Have A Quick Overlook :eyes:")
+    st.text("Step I: Scroll down and use the template to predict your disease based on symptoms.")
+    st.text("Step II: Select exactly five symptoms from the dropdowns.")
+    st.text("Step III: Click Submit to see the prediction.\n")
+    st.markdown("That's cool :sunglasses:")
+    st.info("Note: You will see the approximate disease matching your symptoms.")
     
-    form=st.form(key="MDP")
-    psymptoms = []
-    Symptom1 = form.selectbox('Symptom1', tuple(['None']) + tuple(l1))
-    psymptoms.append(Symptom1)
-    Symptom2 = form.selectbox('Symptom2', tuple(['None']) + tuple(l1))
-    psymptoms.append(Symptom2)
-    Symptom3 = form.selectbox('Symptom3', tuple(['None']) + tuple(l1))
-    psymptoms.append(Symptom3)
-    Symptom4 = form.selectbox('Symptom4', tuple(['None']) + tuple(l1))
-    psymptoms.append(Symptom4)
-    Symptom5 = form.selectbox('Symptom5', tuple(['None']) + tuple(l1))
-    psymptoms.append(Symptom5)
-    
-    Submitted = form.form_submit_button("Submit")
-    
-    if Submitted:
-        if psymptoms.count('None') == 0:
-            for k in range(0, len(l1)):
-                for z in psymptoms:
-                    if (z == l1[k]):
-                        l2[k] = 1
-            inputtest = [l2]
-            predict = gnb.predict(inputtest)
-            predicted = predict[0]
-            h = 'no'
-            for a in range(0, len(disease)):
-                if (disease[predicted] == disease[a]):
-                    h = 'yes'
-                    break
-            if (h == 'yes'):
-                st.success(disease[a])
-            else:
-                st.success("No Disease")
-        elif psymptoms.count('None') >= 1:
-            st.warning("Select All the 5 Symptoms")
-        
+    l1 = ['itching', 'skin_rash', 'nodal_skin_eruptions', 'continuous_sneezing', 'shivering', 'chills', 
+          'joint_pain', 'stomach_pain', 'acidity', 'ulcers_on_tongue', 'muscle_wasting', 'vomiting', 
+          'burning_micturition', 'spotting_ urination', 'fatigue', 'weight_gain', 'anxiety', 'cold_hands_and_feets', 
+          'mood_swings', 'weight_loss', 'restlessness', 'lethargy', 'patches_in_throat', 'irregular_sugar_level', 
+          'cough', 'high_fever', 'sunken_eyes', 'breathlessness', 'sweating', 'dehydration', 'indigestion', 
+          'headache', 'yellowish_skin', 'dark_urine', 'nausea', 'loss_of_appetite', 'pain_behind_the_eyes', 
+          'back_pain', 'constipation', 'abdominal_pain', 'diarrhoea', 'mild_fever', 'yellow_urine', 
+          'yellowing_of_eyes', 'acute_liver_failure', 'fluid_overload', 'swelling_of_stomach', 'swelled_lymph_nodes', 
+          'malaise', 'blurred_and_distorted_vision', 'phlegm', 'throat_irritation', 'redness_of_eyes', 'sinus_pressure', 
+          'runny_nose', 'congestion', 'chest_pain', 'weakness_in_limbs', 'fast_heart_rate', 'pain_during_bowel_movements', 
+          'pain_in_anal_region', 'bloody_stool', 'irritation_in_anus', 'neck_pain', 'dizziness', 'cramps', 'bruising', 
+          'obesity', 'swollen_legs', 'swollen_blood_vessels', 'puffy_face_and_eyes', 'enlarged_thyroid', 'brittle_nails', 
+          'swollen_extremeties', 'excessive_hunger', 'extra_marital_contacts', 'drying_and_tingling_lips', 'slurred_speech', 
+          'knee_pain', 'hip_joint_pain', 'muscle_weakness', 'stiff_neck', 'swelling_joints', 'movement_stiffness', 
+          'spinning_movements', 'loss_of_balance', 'unsteadiness', 'weakness_of_one_body_side', 'loss_of_smell', 
+          'bladder_discomfort', 'foul_smell_of urine', 'continuous_feel_of_urine', 'passage_of_gases', 'internal_itching', 
+          'toxic_look_(typhos)', 'depression', 'irritability', 'muscle_pain', 'altered_sensorium', 'red_spots_over_body', 
+          'belly_pain', 'abnormal_menstruation', 'dischromic _patches', 'watering_from_eyes', 'increased_appetite', 'polyuria', 
+          'family_history', 'mucoid_sputum', 'rusty_sputum', 'lack_of_concentration', 'visual_disturbances', 
+          'receiving_blood_transfusion', 'receiving_unsterile_injections', 'coma', 'stomach_bleeding', 'distention_of_abdomen', 
+          'history_of_alcohol_consumption', 'fluid_overload', 'blood_in_sputum', 'prominent_veins_on_calf', 'palpitations', 
+          'painful_walking', 'pus_filled_pimples', 'blackheads', 'scurring', 'skin_peeling', 'silver_like_dusting', 
+          'small_dents_in_nails', 'inflammatory_nails', 'blister', 'red_sore_around_nose', 'yellow_crust_ooze']
 
-    
-    
-    """"
-    psymptoms = []
-    Symptom1 = st.selectbox('Symptom1', tuple(['None']) + tuple(l1))
-    psymptoms.append(Symptom1)
-    Symptom2 = st.selectbox('Symptom2', tuple(['None']) + tuple(l1))
-    psymptoms.append(Symptom2)
-    Symptom3 = st.selectbox('Symptom3', tuple(['None']) + tuple(l1))
-    psymptoms.append(Symptom3)
-    Symptom4 = st.selectbox('Symptom4', tuple(['None']) + tuple(l1))
-    psymptoms.append(Symptom4)
-    Symptom5 = st.selectbox('Symptom5', tuple(['None']) + tuple(l1))
-    psymptoms.append(Symptom5)
+    traindf, testdf, disease_map = load_data()
+    gnb = train_model(traindf, l1)
+    disease_list = list(disease_map.keys())
 
-    if st.button('Submit'):
-        if psymptoms.count('None') == 0:
-            for k in range(0, len(l1)):
-                for z in psymptoms:
-                    if (z == l1[k]):
-                        l2[k] = 1
-            inputtest = [l2]
-            predict = gnb.predict(inputtest)
-            predicted = predict[0]
-            h = 'no'
-            for a in range(0, len(disease)):
-                if (disease[predicted] == disease[a]):
-                    h = 'yes'
-                    break
-            if (h == 'yes'):
-                st.success(disease[a])
-            else:
-                st.success("No Disease")
-        elif psymptoms.count('None') >= 1:
-            st.warning("Select All the 5 Symptoms")
-    """
-    
+    form = st.form(key="MDP")
+    psymptoms = form.multiselect("Select exactly 5 Symptoms", options=l1)
+    submitted = form.form_submit_button("Submit")
+
+    if submitted:
+        if len(psymptoms) != 5:
+            st.warning("Please select exactly 5 symptoms.")
+        else:
+            input_vector = [1 if symptom in psymptoms else 0 for symptom in l1]
+            predicted_index = gnb.predict([input_vector])[0]
+            st.success(f"Predicted Disease: {disease_list[predicted_index]}")
