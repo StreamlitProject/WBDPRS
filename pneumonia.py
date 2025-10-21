341887c65f85d6fd0dfe6a726838ac63ce836fee
StreamlitProject <100086766+StreamlitProject@users.noreply.github.com>
2025-10-21 17:03:28 +0530

    Update pneumonia.py

--- pneumonia.py
+++ pneumonia.py
@@ -4,7 +4,6 @@ from keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
 from PIL import Image
-from streamlit_option_menu import option_menu
 
 def pneumonia():
     st.markdown("## Have a Quick Overlook :eyes:")
@@ -14,56 +13,29 @@ def pneumonia():
     st.markdown("That's cool :sunglasses:")
     st.info("Note: This is an approximate model under development.")
 
-    # Load model once
-    model = load_model(r'model_vgg16.h5')
+    model = load_model('model_vgg16.h5')
 
-    # Option menu for input type
-    selected_input = option_menu(
-        None,
-        ['Camera', 'Upload Image'],
-        icons=['camera', 'image'],
-        menu_icon="cast",
-        default_index=0,
-        orientation="horizontal",
-        styles={
-            "container": {"padding": "0!important", "background-color": "#fafafa"},
-            "icon": {"color": "black", "font-size": "15px"},
-            "nav-link": {"font-size": "15px", "text-align": "left", "margin":"0px",
-                         "--hover-color": "#eee","--text-color":"#262730"},
-            "nav-link-selected": {"background-color": "#6cdacf","--text-color":"#262730"},
-        }
-    )
+    selected_input = st.radio("Select Input Method", ['Camera', 'Upload Image'], horizontal=True)
 
-    # Function to process and predict
     def predict_image(img):
-        img = img.resize((224, 224)).convert('RGB')
+        img = img.resize((224,224)).convert('RGB')
         x = image.img_to_array(img)
         x = np.expand_dims(x, axis=0)
         x = preprocess_input(x)
         classes = model.predict(x)
+        pred_idx = int(np.argmax(classes[0]))
+        st.success("Normal" if pred_idx==0 else "Pneumonia")
 
-        # Handling prediction safely
-        if classes.shape[1] == 2:
-            pred_idx = np.argmax(classes[0])
-            if pred_idx == 0:
-                st.success("Normal")
-            else:
-                st.success("Pneumonia")
-        else:
-            st.warning("Unexpected model output shape.")
-
-    # Camera input
     if selected_input == 'Camera':
         picture = st.camera_input("Take a picture")
-        if picture is not None:
+        if picture:
             img = Image.open(picture)
             st.image(img, width=500)
             predict_image(img)
 
-    # Upload image
-    elif selected_input == 'Upload Image':
-        uploaded_file = st.file_uploader("Choose a file", type=['png','jpg','jpeg'])
-        if uploaded_file is not None:
+    else:
+        uploaded_file = st.file_uploader("Choose an image", type=['png','jpg','jpeg'])
+        if uploaded_file:
             img = Image.open(uploaded_file)
             st.image(img, width=500)
             predict_image(img)
