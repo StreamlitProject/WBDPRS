af484fbf15a5d807b1af7373c2e167272ad941dc
balajishiva2001 <balajishiva2001@gmail.com>
2026-06-21 03:08:46 +0530

    fix: use width='stretch' to center logo in columns

--- pages/home.py
+++ pages/home.py
@@ -46,7 +46,7 @@ def about_models_dialog():
 
 logo_col1, logo_col2, logo_col3 = st.columns([1, 1, 1])
 with logo_col2:
-    st.image("assets/logo.svg", width=64)
+    st.image("assets/logo.svg", width="stretch")
 
 st.markdown("""
 <div class="page-header">
